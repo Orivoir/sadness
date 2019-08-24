b384cdbe33f5ad4c3946bab85868c8397942851b
= <=>
2019-08-24 19:51:36 +0200

    Updates

---
+++ precache-manifest.4f5bfaf90715ee90442e9b7a332fc90e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8330c1fafde606bb77b1c9e7b21107",
+    "url": "/sadness/index.html"
+  },
+  {
+    "revision": "f98e8ca9adbe7d06f145",
+    "url": "/sadness/static/css/2.47fb9b64.chunk.css"
+  },
+  {
+    "revision": "84b5aa81b7dd4289507f",
+    "url": "/sadness/static/css/main.a9d3bc79.chunk.css"
+  },
+  {
+    "revision": "f98e8ca9adbe7d06f145",
+    "url": "/sadness/static/js/2.e7aa20e3.chunk.js"
+  },
+  {
+    "revision": "84b5aa81b7dd4289507f",
+    "url": "/sadness/static/js/main.072e2a99.chunk.js"
+  },
+  {
+    "revision": "caaf620656253edcd83c",
+    "url": "/sadness/static/js/runtime~main.7163046d.js"
+  },
+  {
+    "revision": "a7e7dd32cb9929b177757a0d7745f2f5",
+    "url": "/sadness/static/media/child_banner.a7e7dd32.jpg"
+  },
+  {
+    "revision": "c82d5f8c559e48bbdbc4f4bd90e39e4b",
+    "url": "/sadness/static/media/compass.c82d5f8c.svg"
+  }
+]);
